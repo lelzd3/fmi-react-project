c46028c4c56415d48562119fe1af3e8ae5b1ab04
Stanislav Klimov <stanislavkl1996@gmail.com>
2020-09-09 00:17:35 +0300

    UserHome changed to Class component

--- client/src/views/UserHome.js
+++ client/src/views/UserHome.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const UserHome = (props) => {
-	return (
-		<div className='UserHome'>
-			<h1>Welcome to the UserHome!</h1>
-		</div>
-	)
+class UserHome extends React.Component {
+
+	render() {
+		return (
+			<div className='UserHome'>
+				<h1>Welcome to the UserHome!</h1>
+			</div>
+		)
+	}
 }
 
 export default UserHome
