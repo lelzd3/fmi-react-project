e0ac8bd07599691da12e8e2553a2e5e7621f50a6
Stanislav Klimov <stanislavkl1996@gmail.com>
2020-09-09 00:02:15 +0300

    Added users frontend

---
+++ client/src/views/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = (props) => {
+	return (
+		<div className='About'>
+			<h1>Payments tracker is a project for Faculty of Mathematics and informatics</h1>
+            <h2>Course: Development of Fullstack applications with Node.js + Express.js + React.js</h2>
+            <h3>Created by Stanko1v9</h3>
+            <h6>In the last possible day, because he is an idiot</h6>
+		</div>
+	)
+}
+
+export default About
---
+++ client/src/views/UserHome.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UserHome = (props) => {
+	return (
+		<div className='UserHome'>
+			<h1>Welcome to the UserHome!</h1>
+		</div>
+	)
+}
+
+export default UserHome
